c4d10962c555d06eeb7f56aa00e63c61dc908fcd
Mikołaj Pich <m.pich@outlook.com>
2019-06-17 21:14:21 +0200

    Configure logging

--- app/src/main/java/io/github/mklkj/filmowy/FilmowyApp.kt
+++ app/src/main/java/io/github/mklkj/filmowy/FilmowyApp.kt
@@ -3,8 +3,15 @@ package io.github.mklkj.filmowy
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import io.github.mklkj.filmowy.di.DaggerAppComponent
+import timber.log.Timber
 
 class FilmowyApp : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
