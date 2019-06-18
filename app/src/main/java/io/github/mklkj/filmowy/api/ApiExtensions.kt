d5da1260ed82a6c072c49e23cd34962c3b0535b2
Mikołaj Pich <m.pich@outlook.com>
2019-06-18 22:12:44 +0200

    Add film images demo

--- app/src/main/java/io/github/mklkj/filmowy/api/ApiExtensions.kt
+++ app/src/main/java/io/github/mklkj/filmowy/api/ApiExtensions.kt
@@ -1,7 +1,15 @@
 package io.github.mklkj.filmowy.api
 
+import android.net.Uri
 import com.google.gson.JsonArray
 
-fun JsonArray.getNullable(index: Int) = get(index).let { if(it.isJsonNull) null else it }
+fun JsonArray.getNullable(index: Int) = get(index).let { if (it.isJsonNull) null else it }
 
 fun String.asMethod(vararg params: Int) = "$this ${params.joinToString(",", "[", "]")}\n"
+
+fun String.getImageUrl(width: Int = 90): Uri = ("https://ssl-gfx.filmweb.pl/ph" + when (width) {
+    in 0..90 -> this
+    in 91..180 -> replace("0.jpg", "2.jpg")
+    in 181..450 -> replace("0.jpg", "3.jpg")
+    else -> replace("0.jpg", "1.jpg")
+}).run { Uri.parse(this) }
--- app/src/main/java/io/github/mklkj/filmowy/ui/MainActivity.kt
+++ app/src/main/java/io/github/mklkj/filmowy/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package io.github.mklkj.filmowy.ui
 
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import dagger.android.support.DaggerAppCompatActivity
 import io.github.mklkj.filmowy.R
 import io.github.mklkj.filmowy.api.FilmRepository
+import io.github.mklkj.filmowy.api.getImageUrl
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
@@ -12,23 +15,35 @@ import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
+    private val disposable: CompositeDisposable = CompositeDisposable()
+
     @Inject
     lateinit var filmRepository: FilmRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var i = 0
+        reloadImage(i)
+        fab.setOnClickListener { reloadImage(++i) }
+    }
 
-        fab.setOnClickListener {
-            filmRepository.getFilmImages(771634, 0)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    container.text = it.toString()
-                }) {
-                    Timber.e(it)
-                    container.text = it.localizedMessage
-                }
-        }
+    private fun reloadImage(index: Int) {
+        disposable.clear()
+        disposable.add(filmRepository.getFilmImages(771634, 0)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Picasso.get().load(it[index].imagePath.getImageUrl(500)).into(image)
+                container.text = "$index: " + it[index].persons?.joinToString { it.personName }
+            }) {
+                Timber.e(it)
+                container.text = it.localizedMessage
+            })
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        disposable.clear()
     }
 }
