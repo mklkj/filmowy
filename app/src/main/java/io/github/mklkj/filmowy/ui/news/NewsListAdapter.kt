5c3c2fdb88adc9329c9ba35b49d12897a5a4e7cc
Mikołaj Pich <m.pich@outlook.com>
2019-07-03 00:04:07 +0200

    Add intent to news onclick

--- app/src/main/java/io/github/mklkj/filmowy/ui/news/NewsListAdapter.kt
+++ app/src/main/java/io/github/mklkj/filmowy/ui/news/NewsListAdapter.kt
@@ -1,5 +1,8 @@
 package io.github.mklkj.filmowy.ui.news
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -7,7 +10,6 @@ import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import io.github.mklkj.filmowy.R
-import io.github.mklkj.filmowy.api.getNewsImageUrl
 import io.github.mklkj.filmowy.api.pojo.NewsLead
 import io.github.mklkj.filmowy.databinding.ItemNewsBinding
 import javax.inject.Inject
@@ -17,7 +19,7 @@ class NewsListAdapter @Inject constructor() : PagedListAdapter<NewsLead, Recycle
     override fun getItemViewType(position: Int) = R.layout.item_news
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), viewType, parent, false))
+        return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), viewType, parent, false), parent.context)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -36,9 +38,15 @@ class NewsListAdapter @Inject constructor() : PagedListAdapter<NewsLead, Recycle
         }
     }
 
-    class ViewHolder(private val binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: ItemNewsBinding, private val context: Context) : RecyclerView.ViewHolder(binding.root) {
 
         fun bindTo(news: NewsLead?) {
+            binding.itemNewsContainer.setOnClickListener {
+                Intent(Intent.ACTION_VIEW).let { intent ->
+                    intent.data = Uri.parse("https://www.filmweb.pl/news/-${news?.id}")
+                    context.startActivity(intent)
+                }
+            }
             binding.item = news
             binding.executePendingBindings()
         }
