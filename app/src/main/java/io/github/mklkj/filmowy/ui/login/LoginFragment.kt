4d60b09634f74188546a056a8bdbb45947059333
Mikołaj Pich <m.pich@outlook.com>
2019-08-03 01:00:12 +0200

    Hide keyboard on login loading

--- app/src/main/java/io/github/mklkj/filmowy/ui/login/LoginFragment.kt
+++ app/src/main/java/io/github/mklkj/filmowy/ui/login/LoginFragment.kt
@@ -1,9 +1,12 @@
 package io.github.mklkj.filmowy.ui.login
 
+import android.app.Activity
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -11,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import dagger.android.support.DaggerFragment
 import io.github.mklkj.filmowy.NavGraphDirections
 import io.github.mklkj.filmowy.R
+import io.github.mklkj.filmowy.api.NetworkState
 import io.github.mklkj.filmowy.databinding.FragmentLoginBinding
 import io.github.mklkj.filmowy.viewmodel.ViewModelFactory
 import javax.inject.Inject
@@ -29,6 +33,9 @@ class LoginFragment : DaggerFragment() {
         return DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = vm
+            vm.networkState.observe(viewLifecycleOwner, Observer {
+                if (it.status == NetworkState.LOADING.status) activity?.hideSoftInput()
+            })
             vm.user.observe(viewLifecycleOwner, Observer {
                 it?.let {
                     activity?.run { navigationLoginHelper.updateNavigationHeader(findViewById(R.id.navView)) }
@@ -36,5 +43,11 @@ class LoginFragment : DaggerFragment() {
                 }
             })
         }.root
+    }
+
+    private fun Activity.hideSoftInput() {
+        (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?)?.run {
+            hideSoftInputFromWindow(window.decorView.applicationWindowToken, 0)
+        }
     }
 }
