f4376259bf4236e444a5d0712b60bc98dc1b6fb3
Mikołaj Pich <m.pich@outlook.com>
2019-08-21 01:38:48 +0200

    Workaround error with response interceptor in scrapper service

--- app/src/main/java/io/github/mklkj/filmowy/api/ApiModule.kt
+++ app/src/main/java/io/github/mklkj/filmowy/api/ApiModule.kt
@@ -20,7 +20,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 import java.util.concurrent.TimeUnit
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -28,46 +27,40 @@ class ApiModule {
 
     @Singleton
     @Provides
-    fun provideApiService(@Named("json") retrofit: Retrofit): ApiService = retrofit.create()
-
-    @Singleton
-    @Provides
-    fun provideScrapperService(@Named("scrapper") retrofit: Retrofit): ScrapperService = retrofit.create()
-
-    @Singleton
-    @Provides
-    @Named("json")
-    fun provideRetrofitJson(client: OkHttpClient.Builder): Retrofit = Retrofit.Builder()
+    fun provideApiService(cookieJar: CookieJar): ApiService = Retrofit.Builder()
         .baseUrl("https://www.filmweb.pl/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
         .client(
-            client
+            OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .followRedirects(true)
+                .callTimeout(30, TimeUnit.SECONDS)
+                .addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                    if (BuildConfig.DEBUG) level = HttpLoggingInterceptor.Level.BODY
+                })
                 .addInterceptor(SignatureInterceptor())
                 .addInterceptor(ResponseInterceptor()).build()
         )
         .build()
+        .create()
 
     @Singleton
     @Provides
-    @Named("scrapper")
-    fun provideRetrofitScrapper(client: OkHttpClient.Builder): Retrofit = Retrofit.Builder()
+    fun provideScrapperService(cookieJar: CookieJar): ScrapperService = Retrofit.Builder()
         .baseUrl("https://www.filmweb.pl/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(JspoonConverterFactory.create())
-        .client(client.build())
+        .client(OkHttpClient.Builder()
+            .cookieJar(cookieJar)
+            .followRedirects(true)
+            .callTimeout(30, TimeUnit.SECONDS)
+            .addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) level = HttpLoggingInterceptor.Level.BODY
+            }).build())
         .build()
-
-    @Singleton
-    @Provides
-    fun provideOkHttpBuilder(cookieJar: CookieJar): OkHttpClient.Builder = OkHttpClient.Builder()
-        .cookieJar(cookieJar)
-        .followRedirects(true)
-        .callTimeout(30, TimeUnit.SECONDS)
-        .addNetworkInterceptor(HttpLoggingInterceptor().apply {
-            if (BuildConfig.DEBUG) level = HttpLoggingInterceptor.Level.BODY
-        })
+        .create()
 
     @Singleton
     @Provides
