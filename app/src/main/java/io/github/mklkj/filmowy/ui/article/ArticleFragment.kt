5240a4cc24bf494ff0058bd60ffc28385b373e07
Mikołaj Pich <m.pich@outlook.com>
2019-08-28 15:29:28 +0200

    Use FragmentArticleBinding directly

--- app/src/main/java/io/github/mklkj/filmowy/ui/article/ArticleFragment.kt
+++ app/src/main/java/io/github/mklkj/filmowy/ui/article/ArticleFragment.kt
@@ -1,10 +1,10 @@
 package io.github.mklkj.filmowy.ui.article
 
 import android.content.Intent
+import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
@@ -12,41 +12,36 @@ import androidx.transition.TransitionInflater
 import dagger.android.support.DaggerFragment
 import io.github.mklkj.filmowy.R
 import io.github.mklkj.filmowy.databinding.FragmentArticleBinding
-import io.github.mklkj.filmowy.ui.HtmlImageGetter
 import io.github.mklkj.filmowy.viewmodel.ViewModelFactory
 import javax.inject.Inject
 
 class ArticleFragment : DaggerFragment() {
 
     @Inject
-    lateinit var htmlImageGetter: HtmlImageGetter
-
-    @Inject
     lateinit var vmFactory: ViewModelFactory
 
-    private val vm: ArticleViewModel by viewModels { vmFactory }
+    private val viewModel: ArticleViewModel by viewModels { vmFactory }
 
     private val args: ArticleFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vm.loadArticle(args.article)
+        viewModel.loadArticle(args.article)
         sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
     }
 
     @Suppress("DEPRECATION")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
-        val binding = DataBindingUtil.inflate<FragmentArticleBinding>(inflater, R.layout.fragment_article, container, false).apply {
+        return FragmentArticleBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
-            viewModel = vm
+            vm = viewModel
+
             article = args.article
-        }
+            articleImage.transitionName = "news_image_${args.position}"
 
-        binding.articleImage.transitionName = "news_image_${args.position}"
-        vm.article.observe(viewLifecycleOwner, Observer { binding.article = it })
-
-        return binding.root
+            viewModel.article.observe(viewLifecycleOwner, Observer { article = it })
+        }.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -58,10 +53,7 @@ class ArticleFragment : DaggerFragment() {
         return when (item.itemId) {
             R.id.article_open_in_browser -> {
                 startActivity(
-                    Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("https://m.filmweb.pl/news/${Uri.encode(args.article.title)}-${args.article.newsId}")
-                    )
+                    Intent(ACTION_VIEW, Uri.parse("https://m.filmweb.pl/news/${Uri.encode(args.article.title)}-${args.article.newsId}"))
                 )
                 true
             }
